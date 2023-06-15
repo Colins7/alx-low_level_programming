2bf826e573108df9ddb850bcd64f95e331e809a8
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-06-15 10:15:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * puts function program
- * main * Aprogram that prints a line with puts function
+ * program: puts function
+ * main : Aprogram that prints a line with puts function
+ * description: this program demostrates a simple C program for put function
  *Return;0
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
