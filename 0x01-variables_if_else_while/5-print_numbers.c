ad312f4865df7447de9d8a74fe9151bfc3a7b5f6
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-06-17 04:50:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - all single digit numbers , print
+*
+* Return: (0) success
+*
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d, digit);
+		++digit;
+	}
+	printf("\n");
+	return (0);
+}
