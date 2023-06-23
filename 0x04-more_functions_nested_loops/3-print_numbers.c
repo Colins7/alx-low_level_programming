ca8891157e9ba7228f661aecd1a216767cd1156d
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-06-23 06:45:49 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_numbers - numbers from 0 to 9, print
  *
- * Return: (0) success
- *
+ * Return: 0
  */
 
 void print_numbers(void)
