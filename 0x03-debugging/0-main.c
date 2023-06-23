db2350eb73f094ad4d71c68066370c4dbeb8d267
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-06-23 05:21:03 -0700

    1-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,19 @@
-#include "main.h"
+#include "main"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests a function that prints if an integer is positive or negative
+ *
+ * Return: 0
+ *
+ */
 
 int main(void)
 {
 int i;
+
 i = 0;
+
 positive_or_negative(i);
+
 return (0);
 }
