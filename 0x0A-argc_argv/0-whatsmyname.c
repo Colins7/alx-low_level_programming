fb998b254258104cc75f94513dfd99b0703cf9b2
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-07-07 09:20:01 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: the number of command lines
+ * @argv: array of strings containing command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
