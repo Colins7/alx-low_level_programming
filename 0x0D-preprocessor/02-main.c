c8b1ada1a09fca7224ef01051579d5287231e4b1
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-07-18 13:20:47 -0700

    2-main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point in the program
+ * main - print the name of the file
  *
- * It uses the predefined macro __FILE__  to retrieve the file
  * Return: 0 success
- *
  */
 
 int main(void)
 {
-printf("Compiled from file: %s\n", __FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
