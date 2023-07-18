c0f256bb706b7028dfe656c231a3a9f2b9674fae
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-07-18 12:03:28 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure representing a dog
+ * @name: pointer to the name of the dog
+ * @age : age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
