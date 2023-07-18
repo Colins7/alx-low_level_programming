9675168099f46c292f802f7c008a63878e88f981
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-07-18 13:24:25 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO-H
-
-/**
- * ABS - macro to compute the absolute value of a number
- * @x - input number
- *
- * Return: absolute value of 'x'
- *
- */
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) (((x) < 0) ? -(x) : (x))
 
