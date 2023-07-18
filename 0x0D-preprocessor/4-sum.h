a9ace59da9703ed1223e99223e25eb43b2a353f3
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-07-18 13:29:00 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,21 +1,6 @@
-#include <stdio.h>
+#ifndef SUM_H
+#define SUM_H
 
-/**
- * SUM - macro to compute sum of two numbers
- * @x: first number
- * @y: second number
- *
- * Return: sum of 'x' and 'y'
- *
- */
+#define SUM(x, y)((x) + (y))
 
-#define SUM(x, y) ((x) + (y))
-
-int main(void)
-{
-int num1 = 98;
-int num2 = 1024;
-int result = SUM(num1, num2);
-printf("The sum of %d and %d is %d\n". num1, num2, result);
-return (0);
-}
+#endif
