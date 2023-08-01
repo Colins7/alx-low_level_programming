42ac5f5c81aa4deacb503a5f10490bc65f0b9b71
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-08-01 14:45:19 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: A pointer to the newly created dog_t structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+int name_len = strlen(name) + 1;
+int owner_len = strlen(owner) + 1;
+dog_t *new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+new_dog->name = malloc(name_len);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+strcpy(new_dog->name, name);
+new_dog->owner = malloc(owner_len);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+strcpy(new_dog->owner, owner);
+new_dog->age = age;
+return (new_dog);
+}
