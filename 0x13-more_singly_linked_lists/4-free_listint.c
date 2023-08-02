49f6ac11305240db4eb42818e158896fac2500fb
Colins7 <collins.alex@students.jkuat.ac.ke.com>
2023-08-02 02:30:36 -0700

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees the linked list
+ * @h: pointer to the head
+ *
+ */
+
+void free_listint(listint_t *h)
+{
+listint_t **temp;
+while (h)
+{
+temp = h->next;
+free(h);
+h = temp;
+}
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,17 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Return: void
+ * Description: singly linked list node structure
+ *
  */
-
 typedef struct listint_s
 {
 int n;
@@ -21,5 +19,7 @@ struct listint_s *next;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif
